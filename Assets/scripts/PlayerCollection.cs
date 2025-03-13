ea7ff85c32435f4fe44183dab77428c67acc9e1c
nachoboy420 <samuelmicallef05@gmail.com>
2025-03-13 15:30:50 +0100

    added jump and score
    checked player was grounded before jumping

--- Assets/scripts/PlayerCollection.cs
+++ Assets/scripts/PlayerCollection.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class PlayerCollection : MonoBehaviour
 {
+
+
+    private int score =0;
+    public TMP_Text scoreText;
+
+
     private void OnTriggerEnter(Collider other)
     {
         // only destroy if collectable
         Debug.Log(other.tag);
         if (other.CompareTag("collectibile"))
-
-        { Destroy(other.gameObject); 
+        {
+            AddScore(1);
+            Destroy(other.gameObject); 
+       
+        
         }
 
 
+    }
+
+
+    private void AddScore (int points)
+    {
+        score = score + points;
+        scoreText.text = $"<b>Score:</b> {score}";
     }
 
 }
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -5,10 +5,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public int health = 100;
+    //public int health = 100;
     public float speed = 4.5f;
+    public float jumpForce = 5;
     public string hero = "Peter";
-    public bool isAlive = true;
+    // public bool isAlive = true;
 
     public Vector3 direction;
     public Rigidbody playerRb;
@@ -22,13 +23,17 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
-    // Update is called once per frame
-    void Update()
+    // Update is called once per frame using void update 
+    void FixedUpdate()
+    ///improves physics functionality,50 times per frame 
     {
         //transform.Translate(direction * Time.deltaTime * speed);
-            //the dot is there to access a functionality of transform
+        //the dot is there to access a functionality of transform
+        Vector3 velocity = direction * speed;
+        velocity.y = playerRb.linearVelocity.y;
 
-        playerRb.linearVelocity = direction * speed;
+        playerRb.linearVelocity = velocity;
+
     }
 
     private void OnMove(InputValue value)
@@ -41,7 +46,17 @@ public class PlayerMovement : MonoBehaviour
             );
         //asks the inoput system what keys are being pressed
 
-            Debug.Log("My name is" + hero);
+        Debug.Log("My name is" + hero);
 
+    }
+
+private void OnJump( InputValue value) 
+    {
+        bool isGrounded = Physics.Raycast(transform.position,Vector3.down, 1f);
+        if (isGrounded)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            // physics raycast will cast aline that can hit other colliders , if it finds a collider, it returns true, if not , it returns false 
+        }
     }
 }
